29588b63148879bb317f42fcec612255ef278362
RxtBD <920164255@qq.com>
2018-11-02 21:45:06 +0800

    use ExecutorService: 服务端采用线程池; CountDownLatch: 客户端模拟并发

--- netty-learning/src/main/java/com/rxt/common/bio/Server.java
+++ netty-learning/src/main/java/com/rxt/common/bio/Server.java
@@ -13,6 +13,8 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * bio Server端
@@ -25,6 +27,12 @@ public class Server {
     private static int DEFAULT_PORT = 7777;    //默认的端口号
 
     private static ServerSocket serverSocket;    //单例的ServerSocket
+
+    private static ExecutorService service;    //线程池
+
+    static {
+        service = Executors.newFixedThreadPool(100);    //大小为100的线程池
+    }
 
     public static void start() throws IOException {
         start(serverSocket);
@@ -44,12 +52,14 @@ public class Server {
                 Socket socket = serverSocket.accept();
 
                 // 每来一个客户端的请求，就new一个Thread进行处理
-                new Thread(new ServerHandler(socket)).start();
+//                new Thread(new ServerHandler(socket)).start();
+                service.execute(new ServerHandler(socket));    //使用线程池代替new Thread
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
+            service.shutdown();
+
             System.out.println("服务端已关闭");
             if (serverSocket != null) {
                 serverSocket.close();
---
+++ netty-learning/src/main/java/com/rxt/common/bio/TestCountDownLatch.java
@@ -0,0 +1,65 @@
+/**
+ * FileName: Test
+ * Author:   Ren Xiaotian
+ * Date:     2018/11/1 21:49
+ */
+
+package com.rxt.common.bio;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * CountDownLatch 并发访问Server
+ */
+public class TestCountDownLatch {
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Server.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        Thread.sleep(100);    //避免客户端比服务端先启动
+
+        final char[] op = {'+', '-', '*', '/'};
+
+        final Random random = new Random(System.currentTimeMillis());
+
+        int num = 10000;    //并发数
+        CountDownLatch countDownLatch = new CountDownLatch(num);
+
+        for (int i = 0; i < num; i++) {
+            //随机生成表达式
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    String expression = random.nextInt(10) + ""
+                            + op[random.nextInt(4)] + ""
+                            + (random.nextInt(10) + 1);
+                    Client.send(expression);
+                }
+            }).start();
+
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            countDownLatch.countDown();
+        }
+
+        countDownLatch.await();
+
+        System.exit(0);
+
+
+    }
+}
