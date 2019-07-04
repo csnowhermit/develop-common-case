91e8a5b1f84e9c68785685f022b1ec576887d627
RxtBD <920164255@qq.com>
2019-07-04 18:55:43 +0800

    update wxDemo01.java

--- commonUtils/src/main/java/com/rxt/common/redEnvelopes/wxDemo01.java
+++ commonUtils/src/main/java/com/rxt/common/redEnvelopes/wxDemo01.java
@@ -8,41 +8,54 @@ import java.util.Random;
  */
 public class wxDemo01 {
 
-//    /**
-//     * 生成随机的钱数
-//     *
-//     * @param _leftMoneyPackage
-//     * @return
-//     */
-//    public static double getRandomMoney(LeftMoneyPackage _leftMoneyPackage) {
-//        // remainSize 剩余的红包数量
-//        // remainMoney 剩余的钱
-//        if (_leftMoneyPackage.remainSize == 1) {
-//            _leftMoneyPackage.remainSize--;
-//            return (double) Math.round(_leftMoneyPackage.remainMoney * 100) / 100;
-//        }
-//        Random r = new Random();
-//        double min = 0.01; //
-//        double max = _leftMoneyPackage.remainMoney / _leftMoneyPackage.remainSize * 2;
-//        if (max > 6) {
-//            max = 6;
-//        }
-//        double money = r.nextDouble() * max;
-//        money = money <= min ? 0.01 : money;
-//        money = Math.floor(money * 100) / 100;
-//        _leftMoneyPackage.remainSize--;
-//        _leftMoneyPackage.remainMoney -= money;
-//        return money;
-//    }
-//
-//    public static void main(String[] args) {
-//        LeftMoneyPackage moneyPackage = new LeftMoneyPackage();
-//        moneyPackage.remainMoney = 40;
-//        moneyPackage.remainSize = 10;
-//
-//        while (moneyPackage.remainSize != 0) {
-//            System.out.print(Cal.getRandomMoney(moneyPackage) + 6 + "   ");
-//
-//        }
-//    }
+    /**
+     * 生成随机的钱数
+     *
+     * @param _leftMoneyPackage
+     * @return
+     */
+    public static double getRandomMoney(LeftMoneyPackage _leftMoneyPackage) {
+        // remainSize 剩余的红包数量
+        // remainMoney 剩余的钱
+        if (_leftMoneyPackage.remainSize == 1) {
+            _leftMoneyPackage.remainSize--;
+            return (double) Math.round(_leftMoneyPackage.remainMoney * 100) / 100;
+        }
+        Random r = new Random();
+        double min = 0.01; //
+        double max = _leftMoneyPackage.remainMoney / _leftMoneyPackage.remainSize * 2;
+        if (max > 6) {
+            max = 6;
+        }
+        double money = r.nextDouble() * max;
+        money = money <= min ? 0.01 : money;
+        money = Math.floor(money * 100) / 100;
+        _leftMoneyPackage.remainSize--;
+        _leftMoneyPackage.remainMoney -= money;
+        return money;
+    }
+
+    public static void main(String[] args) {
+        LeftMoneyPackage moneyPackage = new LeftMoneyPackage();
+        moneyPackage.remainMoney = 40;
+        moneyPackage.remainSize = 10;
+
+        while (moneyPackage.remainSize != 0) {
+            System.out.print(Cal.getRandomMoney(moneyPackage) + 6 + "   ");
+
+        }
+    }
+
 }
+
+class LeftMoneyPackage{
+    public static int remainMoney;
+    public static int remainSize;
+}
+
+class Cal{
+    public static int getRandomMoney(LeftMoneyPackage moneyPackage){
+        return 1;
+    }
+}
+
