fbf595a730a0035bd0f11cb471668d2018457710
RxtBD <920164255@qq.com>
2019-07-30 10:42:41 +0800

    add: jvm Test

--- netty-learning/src/main/java/com/rxt/common/App.java
+++ netty-learning/src/main/java/com/rxt/common/App.java
@@ -2,12 +2,31 @@ package com.rxt.common;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public Integer IntegerDemo(){
+        Integer i = 0;
+        try{
+            return i;
+        }finally {
+            i = 10;
+            System.out.println("done");
+        }
+    }
+
+    public int intDemo(){
+        int i = 0;
+        try{
+            return i;
+        }finally {
+            i = 10;
+            System.out.println("done");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().IntegerDemo());
+        System.out.println(new App().intDemo());
     }
 }
