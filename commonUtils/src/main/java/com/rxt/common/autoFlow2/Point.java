f562791f4457e6dd54041b75e92c9cdcfce243e4
RxtBD <920164255@qq.com>
2019-08-15 09:24:55 +0800

    update: 更新站台/站厅标识

--- commonUtils/src/main/java/com/rxt/common/autoFlow2/Point.java
+++ commonUtils/src/main/java/com/rxt/common/autoFlow2/Point.java
@@ -6,6 +6,7 @@ package com.rxt.common.autoFlow2;
 public class Point {
     private int X;
     private int Y;
+    private transient Location Z;
 
     public Point() {
     }
@@ -13,11 +14,19 @@ public class Point {
     public Point(Point point) {
         this.X = point.getX();
         this.Y = point.getY();
+        this.Z = point.getZ();
     }
 
     public Point(int x, int y) {
         X = x;
         Y = y;
+        Z = Location.HALL;    //默认在站厅
+    }
+
+    public Point(int x, int y, Location z) {
+        X = x;
+        Y = y;
+        Z = z;
     }
 
     public int getX() {
@@ -38,8 +47,25 @@ public class Point {
         return this;
     }
 
+    public Location getZ() {
+        return Z;
+    }
+
+    public Point setZ(Location z) {
+        Z = z;
+        return this;
+    }
+
     @Override
     public String toString() {
-        return X + "," + Y;
+        return X + "," + Y + "," + Z;
     }
+}
+
+/**
+ * 枚举类型：站台/站厅
+ */
+enum Location{
+    HALL,       //站厅
+    PLATFORM    //站台
 }
