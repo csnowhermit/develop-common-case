8be9acf3f6cae9fe63a1748f5acaf111682e3cfc
RxtBD <920164255@qq.com>
2019-08-16 17:39:22 +0800

    update: 新增RPoint可擴展性設計：新增點位放大倍數字段

--- commonUtils/src/main/java/com/rxt/common/autoFlow2/RPoint.java
+++ commonUtils/src/main/java/com/rxt/common/autoFlow2/RPoint.java
@@ -1,36 +1,103 @@
 package com.rxt.common.autoFlow2;
 
-import com.google.gson.annotations.Expose;
-
 public class RPoint extends Point {
 
     private transient PointTag flag;    //随机点位标识：X方向随机/Y方向随机
+    private transient double randMulti;    //随机倍数
 
     public RPoint(PointTag flag) {
         this.flag = flag;
+        this.randMulti = getMulti();
+    }
+
+    public RPoint(PointTag flag, RandMultiple multi) {
+        this.flag = flag;
+        this.randMulti = getMulti(multi);
     }
 
     public RPoint(PointTag flag, Point point) {
         super(point);
         this.flag = flag;
+        this.randMulti = getMulti();
+    }
+
+    public RPoint(PointTag flag, RandMultiple multi, Point point) {
+        super(point);
+        this.flag = flag;
+        this.randMulti = getMulti(multi);
     }
 
     public RPoint(PointTag flag, int x, int y) {
         super(x, y);
         this.flag = flag;
+        this.randMulti = getMulti();
+    }
+
+    public RPoint(PointTag flag, RandMultiple multi, int x, int y) {
+        super(x, y);
+        this.flag = flag;
+        this.randMulti = getMulti(multi);
     }
 
     public PointTag getFlag() {
         return flag;
     }
 
+    public double getRandMulti() {
+        return randMulti;
+    }
+
+    /**
+     * 默认放大倍数为1.0倍
+     */
+    private static double getMulti() {
+        return 1.0;
+    }
+
+    /**
+     * 转换枚举类型倍数为double倍数
+     *
+     * @param multiple 枚举类型
+     * @return
+     */
+    private static double getMulti(RandMultiple multiple) {
+        double multi = 1.0;
+        switch (multiple) {
+            case ONCE:
+                multi = 1.0;
+                break;
+            case ONCE_HALF:
+                multi = 1.5;
+                break;
+            case DOUBLE:
+                multi = 2.0;
+                break;
+            case DOUBLE_HALF:
+                multi = 2.5;
+                break;
+            case TRIPLE:
+                multi = 3.0;
+                break;
+            default:
+                multi = 1.0;
+        }
+        return multi;
+    }
+
     @Override
     public String toString() {
-        return flag + "," + super.toString();
+        return flag + "," + randMulti + "," + super.toString();
     }
 
     public static void main(String[] args) {
         System.out.println(new RPoint(PointTag.TAG_X, new Point(5, 10)));
+        System.out.println(new RPoint(PointTag.TAG_Y, new Point(5, 10)));
+
+        System.out.println(getMulti(RandMultiple.ONCE_HALF));
+        System.out.println(getMulti(RandMultiple.DOUBLE_HALF));
+
+        System.out.println(new RPoint(PointTag.TAG_X, RandMultiple.DOUBLE_HALF, new Point(5, 10)));
+        System.out.println(new RPoint(PointTag.TAG_Y, RandMultiple.ONCE_HALF, new Point(5, 10)));
         System.out.println(new RPoint(PointTag.TAG_Y, new Point(5, 10)));
     }
 }
@@ -38,8 +105,19 @@ public class RPoint extends Point {
 /**
  * 枚举类型：标识点位是X方向随机还是Y方向随机
  */
-enum PointTag{
+enum PointTag {
     NONE,     //不随机
     TAG_X,    //X方向随机
     TAG_Y     //Y方向随机
+}
+
+/**
+ * 枚举类型：随机倍数
+ */
+enum RandMultiple {
+    ONCE,           //1倍：随机数×1
+    ONCE_HALF,      //1.5倍：随机数×1.5
+    DOUBLE,         //2倍：随机数×2
+    DOUBLE_HALF,    //2.5倍：随机数×2.5
+    TRIPLE          //3倍：随机数×3
 }
